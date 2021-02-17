29a90ddcf35e3578cbc65f3caddd3f0b39f6b00b
unknown <you@example.com>
2021-02-17 19:46:45 +0530

    Added: part time wages of employee

--- dailywage.sh
+++ dailywage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 isFulltime=1
+isParttime=2
 empHr=0
 salaryPerHr=20
-randomNumber=$(($RANDOM%2))
+randomNumber=$(($RANDOM%3))
 if [ $randomNumber -eq $isFulltime ]
 then 
  echo "Employee is Full Time"
+empHr=16
+elif [ $randomNumber -eq $isParttime ]
+then
+ echo "Employee is Part Time"
 empHr=8
 else
  echo "Employee is Absent"
