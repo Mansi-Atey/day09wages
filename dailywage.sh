d7cb7d2e334828e2f6755efc49d39cddacd40107
Mansi-Atey <78947536+Mansi-Atey@users.noreply.github.com>
2021-02-18 09:24:36 +0530

    Update dailywage.sh

--- dailywage.sh
+++ dailywage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 salaryPerDay=100
 salaryPerHr=20
-echo "1:wage of month by day 2:wage of month by hr and day"
+echo "1:wage of month by hr and day 2:wage of month by day"
 read -p "enter 1 0r 2" num
 case $num in "1" )
- monthlyWage=$(($salaryPerDay*20))
-echo "wage of month by day =" $monthlyWage
+ monthlyWage=$(($salaryPerDHr*1000))
+echo "wage of month by hr and day =" $monthlyWage
 ;;
 "2")
- monthlyWage=$(($salaryPerHr*100))
-echo "wage of month by hr and day =" $monthlyWage
+ monthlyWage=$(($salaryPerDay*20))
+echo "wage of month by day =" $monthlyWage
 ;;
 esac
 
